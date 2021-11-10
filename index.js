369da554937d8f2cca11b4f117c5a354e58a0023
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-11-09 20:27:32 -0500

    photoshopped food images and built mouseover hide and reveal functions

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-const pyramidImg = document.querySelector('#pyramid-image')
 
-pyramidImg.addEventListener('mouseover', function (e) {
-pyramidImg.src = 'different image tag'
+const hiddenImages = document.querySelectorAll('.hidden-image-wrapper')
+
+hiddenImages.forEach(image => {
+    image.addEventListener('mouseover', becomeVisible)
+    image.addEventListener('mouseleave', becomeInvisible)
 })
+
+function becomeVisible(e) {
+    e.target.firstChild.style = 'visibility: visible'
+}
+
+function becomeInvisible(e) {
+    e.target.firstChild.style = 'visibility: invisible'
+}
+
+
+
+
+
+// Ran into some trouble here with making the dollar menu items appear when you moused 
+//over them and disappear when you moused off. An element with a visibility: none styling can't
+//be the subject of a mouseover event. First I tried wrapping the hidden img elements in span tags
+// and applying the mouseover event to the parent span and then programatically styling the child elements. 
+// This proved ineffective. Consequently, I have had to add event listeners to each hidden image tag individually;
+// a prospect I had been trying to avoid. Ideally I could have just looped over the img tags and added a generic becomeVisible or 
+// becomeInvisible event listener function. Alas hidden images can't be moused over. I've left the attempts commented out below so 
+// you can at least see that I did attempt to acheieve DRY code. 
+
+// const hiddenImages = document.querySelectorAll('.hidden-image')
+
+// function becomeInvisible(e) {
+//     e.target.style = 'visibility: hidden'
+// }
+
+// function becomeVisible(e) {
+//     e.target.style = 'visibility: visible'
+// }
+
+// hiddenImages.forEach(image => {
+//     image.addEventListener('mouseleave', becomeInvisible)
+// })
+
+// hiddenImages.forEach(image => {
+//     image.addEventListener('mouseenter', becomeVisible)
+// })
