c94b3d57f8e2b3d3781a6995af10aad1a549366d
Gabriel Chazanov <gabe.chaz@gmail.com>
2021-11-10 05:34:15 -0500

    prepush commit

--- index.js
+++ index.js
@@ -4,9 +4,7 @@
 const hiddenImageWrappers = document.querySelectorAll('.hidden-image-wrapper')
 const hiddenImages = document.querySelectorAll('.hidden-image')
 
-hiddenImages.forEach(image => {
-    image.addEventListener('click', addImageClick)
-})
+
 
 hiddenImageWrappers.forEach(image => {
     image.addEventListener('mouseover', becomeVisible)
@@ -17,19 +15,16 @@ function becomeVisible(e) {
     e.target.firstChild.style = 'visibility: visible'
     }
 
-    function clickLink() {
-
-    }
 
 
-function clickInvisible(e) {
-    e.target.firstChild.style = 'visibility: visible'
-}
 
 function becomeInvisible(e) {
-    e.target.firstChild.style = 'visibility: invisible'
+    e.target.firstChild.style = 'visibility: invisible'ū 
 }
 
+
+//This is the function referenced in the readme that mysteriously obfuscated all the styling on the targeted element after this function was
+//invoked. 
 function addImageClick(e) {
 if (e.target.id = 'taco-image') {
     imagesClicked.tacoClicked = true
@@ -40,29 +35,3 @@ if (e.target.id = 'taco-image') {
 
 
 
-// Ran into some trouble here with making the dollar menu items appear when you moused 
-//over them and disappear when you moused off. An element with a visibility: none styling can't
-//be the subject of a mouseover event. First I tried wrapping the hidden img elements in span tags
-// and applying the mouseover event to the parent span and then programatically styling the child elements. 
-// This proved ineffective. Consequently, I have had to add event listeners to each hidden image tag individually;
-// a prospect I had been trying to avoid. Ideally I could have just looped over the img tags and added a generic becomeVisible or 
-// becomeInvisible event listener function. Alas hidden images can't be moused over. I've left the attempts commented out below so 
-// you can at least see that I did attempt to acheieve DRY code. 
-
-// const hiddenImages = document.querySelectorAll('.hidden-image')
-
-// function becomeInvisible(e) {
-//     e.target.style = 'visibility: hidden'
-// }
-
-// function becomeVisible(e) {
-//     e.target.style = 'visibility: visible'
-// }
-
-// hiddenImages.forEach(image => {
-//     image.addEventListener('mouseleave', becomeInvisible)
-// })
-
-// hiddenImages.forEach(image => {
-//     image.addEventListener('mouseenter', becomeVisible)
-// })
